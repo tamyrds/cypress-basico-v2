3c83904b1dd93e09ac989a75436e80c01e11d068
Tamires Rodrigues <tamy.rdasilva@gmail.com>
2022-11-13 21:34:51 -0300

    Atualizando be.equal

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -3,7 +3,8 @@
 
 describe('Central de Atendimento ao Cliente TAT', () => {
   it('verifica o título da aplicação', () => {
-    cy.visit('./src/indext.html')
-    cy.title('Central de Atendimento ao Cliente TAT')
+    cy.visit('./src/index.html')
+    cy.title()
+    .should('be.equal','Central de Atendimento ao Cliente TAT')
   })
 })
