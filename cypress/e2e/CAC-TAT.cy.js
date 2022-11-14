4e42d2a2ae51878993e9407af807bd9a8b2eb6a0
Tamires Rodrigues <tamy.rdasilva@gmail.com>
2022-11-13 22:15:06 -0300

    Preenchido o formulario

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -2,9 +2,31 @@
 
 
 describe('Central de Atendimento ao Cliente TAT', () => {
-  it('verifica o título da aplicação', () => {
+    beforeEach(() => {
+    
     cy.visit('./src/index.html')
     cy.title()
     .should('be.equal','Central de Atendimento ao Cliente TAT')
-  })
+})
+
+  it('Deve preencher o formulario', () => {
+      cy.get('input[id="firstName"]')
+        .type('Tamires Rodrigues')
+        .should('have.value', 'Tamires Rodrigues')
+
+      cy.get('input[id="lastName"]')
+      .type('da Silva')
+      .should('have.value', 'da Silva')
+
+      cy.get('input[id="email"]')
+      .type('teste@teste.com')
+
+      cy.get('textarea[id="open-text-area"]')
+        .type('1234')
+
+      cy.get('button[class="button"]').click()
+      
+      cy.get('span[class="success"]')
+        .should('be.visible','Mensagem enviada com sucesso.')
+  });
 })
