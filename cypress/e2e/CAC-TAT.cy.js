ea5d9cee1eeff4676c911417abc4b528e8c7cff9
Tamires Rodrigues <tamy.rdasilva@gmail.com>
2022-11-16 10:09:49 -0300

    Realizado todos os desafios

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -9,24 +9,89 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     .should('be.equal','Central de Atendimento ao Cliente TAT')
 })
 
-  it('Deve preencher o formulario', () => {
-      cy.get('input[id="firstName"]')
-        .type('Tamires Rodrigues')
-        .should('have.value', 'Tamires Rodrigues')
-
-      cy.get('input[id="lastName"]')
-      .type('da Silva')
-      .should('have.value', 'da Silva')
-
-      cy.get('input[id="email"]')
-      .type('teste@teste.com')
-
-      cy.get('textarea[id="open-text-area"]')
-        .type('1234')
-
-      cy.get('button[class="button"]').click()
+  it.only('Deve preencher o formulario', () => {
+    const TextLong = 'Teste, teste, teste, Teste, teste, testeTeste, teste, testeTeste, teste, testeTeste, teste, testeTeste, teste, teste'
+      cy.fillMandatoryFieldsAndSubmit()
+     
       
-      cy.get('span[class="success"]')
+      cy.contains('button[class="button"]', 'Enviar').click()
         .should('be.visible','Mensagem enviada com sucesso.')
+  });
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    
+    cy.get('input[id="firstName"]')
+      .type('Tamires Rodrigues')
+      .should('have.value', 'Tamires Rodrigues')
+
+    cy.get('input[id="lastName"]')
+    .type('da Silva')
+    
+
+    cy.get('input[id="email"]')
+    .type('teste.com')
+
+    cy.get('textarea[id="open-text-area"]')
+      .type('ewwweewewewewewewew') //Deve tirar o delay de 3s para 1s.
+
+    cy.get('button[class="button"]').click()
+
+    cy.get('.error')
+    .should('be.visible','Valide os campos obrigatórios!')
+  });
+
+  it('Campos telefone só deve aceitar numeros ', () => {
+      cy.get('#phone')
+        .type('testettet')
+        .should('have.value', '')
+  });
+  it('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('input[id="firstName"]')
+      .type('Tamires Rodrigues')
+      .should('have.value', 'Tamires Rodrigues')
+
+    cy.get('input[id="lastName"]')
+    .type('da Silva')
+    
+
+    cy.get('input[id="email"]')
+    .type('teste@teste.com')
+
+    cy.get('input[id="phone-checkbox"]').click()
+
+    cy.get('textarea[id="open-text-area"]')
+      .type('teste teste') 
+
+    cy.get('button[class="button"]').click()
+    
+    cy.get('.error')
+      .should('be.visible','Valide os campos obrigatórios!')
+
+  });
+
+  it('Preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('input[id="firstName"]')
+    .type('Tamires Rodrigues')
+    .should('have.value', 'Tamires Rodrigues')
+  cy.get('input[id="lastName"]')
+  .type('da Silva')
+  cy.get('input[id="email"]')
+  .type('teste@teste.com')
+  cy.get('textarea[id="open-text-area"]')
+    .type('teste teste')
+    .should('have.value', 'teste teste')
+    .clear()
+    .should('have.value', '')
+  
+  });
+
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.get('button[class="button"]').click()
+
+    cy.get('.error')
+    .should('be.visible','Valide os campos obrigatórios!')
+  });
+
+  it('envia o formuário com sucesso usando um comando customizado', () => {
+      
   });
 })
