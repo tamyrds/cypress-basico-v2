8d3739f12e238b2f76346b1fc07a083843a57c52
Tamires Rodrigues <tamy.rdasilva@gmail.com>
2022-11-28 22:39:58 -0300

    view

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  // viewportWidth:370,
+  // viewportHeight:660,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
+
     },
   }
 })
